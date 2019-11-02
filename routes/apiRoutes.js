a7c1aa44054164cd751a0ddab5c26214c96c0caf
Will <Will.irvin18@gmail.com>
2019-11-02 12:52:56 -0400

    post is working, req.body is showing pizzas

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,10 +9,11 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/favorites", function(req, res) {
+    console.log(req.body)
+    // db.Example.create(req.body).then(function(dbExample) {
+    //   res.json(dbExample);
+    // });
   });
 
   // Delete an example by id
